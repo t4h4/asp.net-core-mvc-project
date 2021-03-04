7195758f75e6a726b30c17e6d5692027a18e9d4a
t4h4 <tahayasin@outlook.com>
2021-03-04 03:22:18 +0300

    Static dosyalar eklendi.

--- app.webui/Startup.cs
+++ app.webui/Startup.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace app.webui
@@ -23,6 +25,15 @@ namespace app.webui
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles(); // wwwroot
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(),"node_modules")), // node_modules açılıyor.
+                    RequestPath="/modules"        // dışarıdan bu şekilde çağıracağız.        
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
