3df45e08619dfe52d7d8c3e2f803e2541f43bc95
t4h4 <tahayasin@outlook.com>
2021-03-10 15:23:15 +0300

    Ürün kayıt formu eklendi.

--- app.webui/Controllers/ProductController.cs
+++ app.webui/Controllers/ProductController.cs
@@ -7,55 +7,32 @@ using app.webui.Models;
 
 namespace shopapp.webui.Controllers
 {
-    public class ProductController : Controller
+    public class ProductController:Controller
     {
         public IActionResult Index()
         {
-            var product = new Product { Name = "Iphone X", Price = 6000, Description = "güzel telefon" };
-
-            // ViewData["Category"] = "Telefonlar";
-            // ViewData["Product"] = product;
-
+            var product = new Product {Name="Iphone X",Price=6000,Description="güzel telefon"};
             ViewBag.Category = "Telefonlar";
-            // ViewBag.Product = product;
 
-            // ViewBag.Product
             return View(product);
         }
-        // product/list => tüm ürünleri (sayfalama)
-        // product/list/2 => 2 numaralı kategoriye ait ürünler
-        public IActionResult list(int? id, string q, double? min_price, double? max_price) //string varsayılan olarak nulluble oldugu için soru işareti eklemiyoruz.
+        public IActionResult list(int? id,string q,double? min_price,double? max_price) 
         {
-            // {controller}/{action}/{id?}
-            // product/list/3
-            // RouteData.Values["controller"] => product
-            // RouteData.Values["action"] => list
-            // RouteData.Values["id"] => 3
-
-            // Console.WriteLine(RouteData.Values["controller"]);
-            // Console.WriteLine(RouteData.Values["action"]);
-            // Console.WriteLine(RouteData.Values["id"]);
-
-            // QueryString
-            // Console.WriteLine(q);
-            // Console.WriteLine(HttpContext.Request.Query["q"].ToString());
-            // Console.WriteLine(HttpContext.Request.Query["min_price"].ToString());
-
             var products = ProductRepository.Products;
 
-            if (id != null)
+            if (id!=null)
             {
-                products = products.Where(p => p.CategoryId == id).ToList();
+                products = products.Where(p=>p.CategoryId==id).ToList();
             }
 
             if (!string.IsNullOrEmpty(q))
             {
-                products = products.Where(i => i.Name.Contains(q) || i.Description.Contains(q)).ToList();
+                products = products.Where(i=>i.Name.Contains(q) || i.Description.Contains(q)).ToList();
             }
 
             var productViewModel = new ProductViewModel()
             {
-                Products = products
+                Products =products
             };
 
             return View(productViewModel);
@@ -65,5 +42,13 @@ namespace shopapp.webui.Controllers
         {
             return View(ProductRepository.GetProductById(id));
         }
+
+        public IActionResult Create(string name,double price)
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(price);
+            return View();
+        }
+        
     }
 }
