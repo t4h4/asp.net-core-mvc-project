d8e1453079495786a158de6bbb78544a7e039247
t4h4 <tahayasin@outlook.com>
2021-03-11 03:35:17 +0300

    Form validation olayı eklendi.

--- app.webui/Controllers/ProductController.cs
+++ app.webui/Controllers/ProductController.cs
@@ -6,34 +6,34 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using app.webui.Data;
 using app.webui.Models;
 
-namespace shopapp.webui.Controllers
+namespace app.webui.Controllers
 {
-    public class ProductController:Controller
+    public class ProductController : Controller
     {
         public IActionResult Index()
         {
-            var product = new Product {Name="Iphone X",Price=6000,Description="güzel telefon"};
+            var product = new Product { Name = "Iphone X", Price = 6000, Description = "güzel telefon" };
             ViewBag.Category = "Telefonlar";
 
             return View(product);
         }
-        public IActionResult list(int? id,string q,double? min_price,double? max_price) 
+        public IActionResult list(int? id, string q, double? min_price, double? max_price)
         {
             var products = ProductRepository.Products;
 
-            if (id!=null)
+            if (id != null)
             {
-                products = products.Where(p=>p.CategoryId==id).ToList();
+                products = products.Where(p => p.CategoryId == id).ToList();
             }
 
             if (!string.IsNullOrEmpty(q))
             {
-                products = products.Where(i=>i.Name.Contains(q) || i.Description.Contains(q)).ToList();
+                products = products.Where(i => i.Name.Contains(q) || i.Description.Contains(q)).ToList();
             }
 
             var productViewModel = new ProductViewModel()
             {
-                Products =products
+                Products = products
             };
 
             return View(productViewModel);
@@ -47,22 +47,28 @@ namespace shopapp.webui.Controllers
 
         [HttpGet]
         public IActionResult Create()
-        {           
-            ViewBag.Categories = new SelectList(CategoryRepository.Categories,"CategoryId","Name");
-            return View();
+        {
+            ViewBag.Categories = new SelectList(CategoryRepository.Categories, "CategoryId", "Name");
+            return View(new Product());
         }
 
         [HttpPost]
         public IActionResult Create(Product p)
-        {           
-            ProductRepository.AddProduct(p);
-            return RedirectToAction("list"); 
+        {
+            if (ModelState.IsValid) //validasyon tamam ise,
+            {
+                ProductRepository.AddProduct(p);
+                return RedirectToAction("list");
+            }
+            ViewBag.Categories = new SelectList(CategoryRepository.Categories, "CategoryId", "Name");
+
+            return View(p);
         }
 
         [HttpGet]
         public IActionResult Edit(int id)
         {
-            ViewBag.Categories = new SelectList(CategoryRepository.Categories,"CategoryId","Name");
+            ViewBag.Categories = new SelectList(CategoryRepository.Categories, "CategoryId", "Name");
             return View(ProductRepository.GetProductById(id));
         }
 
@@ -72,6 +78,7 @@ namespace shopapp.webui.Controllers
             ProductRepository.EditProduct(p);
             return RedirectToAction("list");
         }
-        
+
+
     }
 }
